a21f50b9dd915b2d8429a3144970f504faa3b2eb
marcos-grando <168216213+marcos-grando@users.noreply.github.com>
2026-04-19 19:02:40 -0300

    mudança de project supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vwwuvynwhumvylkbtsft.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3d3V2eW53aHVtdnlsa2J0c2Z0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwNzMxNzYsImV4cCI6MjA2MTY0OTE3Nn0.AVeQb6eCPk8FTxRlkiU5pshvNVtTndnvu4pSm5FvAgk';
+const supabaseUrl = 'https://xmwrvhqcsasxgfvbrgjz.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhtd3J2aHFjc2FzeGdmdmJyZ2p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4NjcwMTEsImV4cCI6MjA2NTQ0MzAxMX0.2X3l8Um6vTIMs5b73Nj5Xq3kbwY2Kwee2-Vs0ooiFhI'
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
